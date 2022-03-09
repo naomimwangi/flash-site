e02ec22485835660b103a80e295a0beef42ef9bc
naomimwangi <naomimwangi7@gmail.com>
2022-03-09 21:52:36 +0300

    add functions

---
+++ script.js
@@ -0,0 +1,15 @@
+$('li#javascript').click (function(){
+    $('#javascript p').toggle ('hidden');
+});
+$('li#operators').click (function(){
+    $('#operators p').toggle ('hidden');
+});
+$('li#strings').click(function () {
+    $('#strings p').toggle('hidden');
+});
+$('li#methods').click(function () {
+    $('#methods p').toggle('hidden');
+}); 
+$('li#functions').click(function () {
+    $('#functions p').toggle('hidden');
+});
